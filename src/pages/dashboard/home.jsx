6eeadd92658a34e1268c7c1eedd68928a62d1d19
Himanshu Sahu <109578416+himanshu20163@users.noreply.github.com>
2024-12-23 22:05:45 +0530

    Update home.jsx

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -48,23 +48,24 @@ export function Home() {
           />
         ))}
       </div>
-      <div className="mb-6 grid grid-cols-2 gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
-        {statisticsChartsData.map((props) => (
-          <StatisticsChart
-            key={props.title}
-            {...props}
-            footer={
-              <Typography
-                variant="small"
-                className="flex items-center font-normal text-blue-gray-600"
-              >
-                <ClockIcon strokeWidth={2} className="h-4 w-4 text-blue-gray-400" />
-                &nbsp;{props.footer}
-              </Typography>
-            }
-          />
-        ))}
-      </div>
+       <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3">
+  {statisticsChartsData.map((props) => (
+    <StatisticsChart
+      key={props.title}
+      {...props}
+      footer={
+        <Typography
+          variant="small"
+          className="flex items-center font-normal text-blue-gray-600"
+        >
+          <ClockIcon strokeWidth={2} className="h-4 w-4 text-blue-gray-400" />
+          &nbsp;{props.footer}
+        </Typography>
+      }
+    />
+  ))}
+</div>
+
       {/* <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
         <Card className="overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm">
           <CardHeader
